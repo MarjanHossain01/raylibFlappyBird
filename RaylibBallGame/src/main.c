a04ac2e1c43ce3c7d3b18ffacc28ca0075e58fed
MarjanHossain01 <im.marjan.hossain@gmail.com>
2024-04-24 16:50:18 +0600

    Small changes

--- RaylibBallGame/src/main.c
+++ RaylibBallGame/src/main.c
@@ -24,87 +24,13 @@ Bird bird = {
     .Texture = {0} // Initialize texture
 };
 
-
 // Platform Properties
 typedef struct Platform {
     int x;
     int y;
     int width;
     int height;
-    int velocity;
-    bool active;
 } Platform;
-
-void retry(Platform Top_platforms[], Platform Bottom_platforms[]);
-void exitGame();
-
-void Update_platforms(Platform Top_platforms[], Platform Bottom_platforms[]) {
-    int i;
-    for (i = 0; i < 4; i++) {
-        DrawRectangle(Top_platforms[i].x, Top_platforms[i].y, Top_platforms[i].width, Top_platforms[i].height, GREEN);
-        if (Top_platforms[i].x + Top_platforms[i].width < 0) {
-            Top_platforms[i].x = 820;
-        }
-        DrawRectangle(Bottom_platforms[i].x, 450, Bottom_platforms[i].width, 150, WHITE);
-        if (Bottom_platforms[i].x + Bottom_platforms[i].width < 0) {
-            Bottom_platforms[i].x = 820;
-        }
-    }
-      
-    for(i=0; i<4; i++)
-    {
-        if(death != true)
-        {
-            Top_platforms[i].x -= 4;   
-            Bottom_platforms[i].x -= 4;
-        }
-    }
-}
-
-int scoreUpdate(Platform Top_platforms[], int* score)
-{  
-    static bool crossed[4] = {false}; // Track if the bird has crossed each platform
-    for(int i = 0; i < 4; i++) {
-        if(bird.position.x > Top_platforms[i].x + Top_platforms[i].width &&
-           bird.position.x - bird.width/2 <= Top_platforms[i].x + Top_platforms[i].width &&
-           !crossed[i]) // Check if the bird crossed the platform and hasn't already been counted
-        {
-           (*score)++;
-           crossed[i] = true; // Mark the platform as crossed
-        }
-        else if (bird.position.x <= Top_platforms[i].x + Top_platforms[i].width) {
-            crossed[i] = false; // Reset the crossed flag if the bird is still on the platform
-        }
-    }
-    
-    char scoreText[20];
-    snprintf(scoreText, 20, "Score: %d", *score);
-    DrawText(scoreText, 25, 25, 58, WHITE);
-    
-    return *score;
-}
-
-
-int collision(Platform Top_platforms[], Platform Bottom_platforms[]) {   
-    if((bird.position.y + bird.height/2) >= height){
-        return 1;
-    }
-    if((bird.position.y - bird.height/2) <= 0){
-        return 1;
-    }   
-    
-    for (int i = 0; i < 4; i++) {
-        if (CheckCollisionCircleRec(bird.position, bird.height/2, (Rectangle){Top_platforms[i].x,
-            Top_platforms[i].y, Top_platforms[i].width, Top_platforms[i].height})) {
-            return 1;
-        }
-        if (CheckCollisionCircleRec(bird.position, bird.height/2, (Rectangle){Bottom_platforms[i].x,
-            Bottom_platforms[i].y, Bottom_platforms[i].width, Bottom_platforms[i].height})) {
-            return 1;
-        }
-    }
-    return 0; // No collision detected
-}
 
 typedef struct Button {
     Rectangle rect;
@@ -127,22 +53,112 @@ struct Button Exit = {
     .text = "Exit"
 };
 
-struct Button play = {
-    .rect = {width/2+60, height/2+50, 120, 50},
+struct Button Play = {
+    .rect = {width/2-125, height/2+50, 130, 50},
     .color = RED,
     .textcolor = RAYWHITE,
     .text = "Play"
 };
 
-struct Button High_score = {
-    .rect = {width/2+60, height/2+ 50*2, 120, 50},
-    .color = RED,
-    .textcolor = RAYWHITE,
-    .text = "High Score"
-};
+//Function Declarations
+int mainMenu();
+void Update_platforms(Platform Top_platforms[], Platform Bottom_platforms[]); //Obstacle generation
+int collision(Platform Top_platforms[], Platform Bottom_platforms[]); //Collision detection
+int scoreUpdate(Platform Top_platforms[], int* score); //Score Function
+int gameOver(Platform Top_platforms[], Platform Bottom_platforms[], int* score); // Game over stage
+void retry(Platform Top_platforms[], Platform Bottom_platforms[]); //Retry
+void exitGame(); //Exit
+
+int mainMenu()
+{
+    Texture2D logo = LoadTexture("F:\\Code\\Project\\RaylibBallGame\\assets\\logo.png");
+    DrawTexture(logo, 270, 50, WHITE);
+    
+    DrawRectangleRec(Play.rect, Play.color);
+        DrawText(Play.text, Play.rect.x + Play.rect.width / 2 - MeasureText(Play.text, 20) / 2,
+        Play.rect.y + Play.rect.height / 2 - 10, 20, Play.textcolor);
+        
+   DrawRectangleRec(Exit.rect, Exit.color);
+        DrawText(Exit.text, Exit.rect.x + Exit.rect.width / 2 - MeasureText(Exit.text, 20) / 2,
+        Exit.rect.y + Exit.rect.height / 2 - 10, 20, Exit.textcolor);
+   
+   if(CheckCollisionPointRec(GetMousePosition(), Play.rect) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+   {
+       return 1;
+   }
+   
+   if(CheckCollisionPointRec(GetMousePosition(), Exit.rect) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+   {
+       exitGame();
+   }
+   return 0;
+}
+
+void Update_platforms(Platform Top_platforms[], Platform Bottom_platforms[]) {
+    int i;
+    for (i = 0; i < 4; i++) {
+        DrawRectangle(Top_platforms[i].x, Top_platforms[i].y, Top_platforms[i].width, Top_platforms[i].height, GREEN);
+        if (Top_platforms[i].x + Top_platforms[i].width < 0) { // if platforms go outside of the screen they will be teleported to the right side
+            Top_platforms[i].x = 820;
+        }
+        DrawRectangle(Bottom_platforms[i].x, 450, Bottom_platforms[i].width, 150, WHITE);
+        if (Bottom_platforms[i].x + Bottom_platforms[i].width < 0) {
+            Bottom_platforms[i].x = 820;
+        }
+        if(death != true)
+        {
+            Top_platforms[i].x -= 4;   
+            Bottom_platforms[i].x -= 4;
+        }
+    }
+}
+
+int collision(Platform Top_platforms[], Platform Bottom_platforms[]) {   
+    if((bird.position.y + bird.height/2) >= height){ // if bird touches the bottom of the screen
+        return 1;
+    }
+    if((bird.position.y - bird.height/2) <= 0){ // if bird touches the top of the screen
+        return 1;
+    }   
+    
+    for (int i = 0; i < 4; i++) { // If bird touches any rectangles
+        if (CheckCollisionCircleRec(bird.position, bird.height/2, (Rectangle){Top_platforms[i].x,
+            Top_platforms[i].y, Top_platforms[i].width, Top_platforms[i].height})) {
+            return 1;
+        }
+        if (CheckCollisionCircleRec(bird.position, bird.height/2, (Rectangle){Bottom_platforms[i].x,
+            Bottom_platforms[i].y, Bottom_platforms[i].width, Bottom_platforms[i].height})) {
+            return 1;
+        }
+    }
+    return 0; // No collision detected
+}
+
+int scoreUpdate(Platform Top_platforms[], int* score)
+{  
+    static bool crossed[4] = {false}; // check if the bird has crossed each platform
+    for(int i = 0; i < 4; i++) {
+        if(bird.position.x > Top_platforms[i].x + Top_platforms[i].width &&
+           bird.position.x - bird.width/2 <= Top_platforms[i].x + Top_platforms[i].width &&
+           !crossed[i]) //if the ball passes the rectangle and it already hasnt been crossed, score will go up
+        {
+           (*score)++;
+           crossed[i] = true; // Mark the platform as crossed
+        }
+        else if (bird.position.x <= Top_platforms[i].x + Top_platforms[i].width) {
+            crossed[i] = false; //unmark the check if the bird is still on the platform
+        }
+    }
+    
+    char scoreText[20]; //Display score
+    snprintf(scoreText, 20, "Score: %d", *score);
+    DrawText(scoreText, 25, 25, 58, WHITE);
+    
+    return *score;
+}
 
 int gameOver(Platform Top_platforms[], Platform Bottom_platforms[], int* score) {
-    bird.velocity = (Vector2){0, 0}; // Reset bird's velocity
+    bird.velocity = (Vector2){0, 0}; // Stop bird from moving
     death = true;
     
     DrawText("Game Over!", 250, 250, 62, RAYWHITE);
@@ -182,7 +198,12 @@ void exitGame(){
 }
 
 int main() {
+    InitWindow(width, height, "Ball Game");
+    InitAudioDevice();
+    
     int score = 0;
+    
+    //Initializing Platforms
     Platform Top_platforms[4];
     Platform Bottom_platforms[4];
     for (int i = 0; i < 4; i++) {
@@ -196,45 +217,50 @@ int main() {
         Bottom_platforms[i].width = 70;
         Bottom_platforms[i].height = 150;
     }
-       
-    InitWindow(width, height, "Ball Game");
-    InitAudioDevice();
     
+    //Assets
     Texture2D background = LoadTexture("F:\\Code\\Project\\RaylibBallGame\\assets\\background.png");
     Sound jump = LoadSound("F:\\Code\\Project\\RaylibBallGame\\assets\\boom.mp3");
     bird.Texture = LoadTexture("F:\\Code\\Project\\RaylibBallGame\\assets\\bird.png");
-
+    
+    // Draw the main menu once to display buttons
+    int startGame = mainMenu();
     
     SetTargetFPS(60);
-
     while (!WindowShouldClose()) {
         BeginDrawing();
-
         ClearBackground(BLUE);
         DrawTexture(background, 0, 0, RAYWHITE);
         
-        DrawTexture(bird.Texture, bird.position.x - bird.width/2, bird.position.y - bird.height/2, WHITE);
-        Update_platforms(Top_platforms, Bottom_platforms);
-        collision(Top_platforms, Bottom_platforms);
+        if (startGame == 0) {
+            startGame = mainMenu(); // Update startGame based on menu input
+        }
+        else {
+            DrawTexture(bird.Texture, bird.position.x - bird.width/2, bird.position.y - bird.height/2, WHITE);
         
-        scoreUpdate(Top_platforms, &score);
-        
-        // Setting Bird Properties
-        bird.position.y += bird.velocity.y; 
-        bird.velocity.y += 1; 
-
-        // Game over // Retry and Menu
-        if (collision(Top_platforms, Bottom_platforms)) {
-            gameOver(Top_platforms, Bottom_platforms, &score);
+            Update_platforms(Top_platforms, Bottom_platforms);
+            collision(Top_platforms, Bottom_platforms);
+            
+            scoreUpdate(Top_platforms, &score);
+            
+            // Setting Bird Properties
+            bird.position.y += bird.velocity.y; 
+            bird.velocity.y += 1; 
+    
+            // Game over // Retry and Exit
+            if (collision(Top_platforms, Bottom_platforms)) {
+                gameOver(Top_platforms, Bottom_platforms, &score);
+            }
+            
+            if ((IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_W)) && death != true) {
+                bird.velocity.y = -15;
+                PlaySound(jump);
+            }
         }
         
-        if ((IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_W)) && death != true) {
-          bird.velocity.y = -15;
-          PlaySound(jump);
-        }
-
         EndDrawing();
     }
+    
     CloseWindow();
     return 0;
-} 
+}
